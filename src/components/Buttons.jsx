e0424d8dff2af581a071c7969962d6556b97a077
RishabhChowdhry <rishabhchowdhry.99@gmail.com>
2023-11-07 07:23:07 +0500

    struct

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+const Buttons = ({ title, onCLick, disabled, onhover }) => {
+  return <div></div>;
+};
+
+export default Buttons;
